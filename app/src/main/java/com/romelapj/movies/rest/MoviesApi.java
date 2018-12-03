86f71fd732c4fb0ffbd155f7383da56ceba5e292
romelapj <romelpalomino@hotmail.com>
2018-12-03 18:56:04 -0500

    add class and remove key

--- app/src/main/java/com/romelapj/movies/rest/MoviesApi.java
+++ app/src/main/java/com/romelapj/movies/rest/MoviesApi.java
@@ -7,10 +7,10 @@ import retrofit2.http.GET;
 
 public interface MoviesApi {
 
-    @GET("popular?api_key=906feb0183f849429a531024630562ab")
+    @GET("popular?api_key=API_KEY")
     public Single<MoviesResponse> getPopularMovies();
 
-    @GET("top_rated?api_key=906feb0183f849429a531024630562ab")
+    @GET("top_rated?api_key=API_KEY")
     public Single<MoviesResponse> getToRatedMovies();
 
 }
